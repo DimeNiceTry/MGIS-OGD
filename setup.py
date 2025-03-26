7c09f806c63a42e1eddd998a9ba3c4625dfc7f4f
DimeNiceTry <chdd04@gmail.com>
2025-03-26 10:54:10 +0300

    fix

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mgis-ogd",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "Django==5.0.2",
+        "djangorestframework==3.14.0",
+        "django-cors-headers==4.3.1",
+        "gunicorn==21.2.0",
+        "requests==2.31.0",
+        "python-dotenv==1.0.1",
+        "drf-yasg>=1.21.7",
+    ],
+) 
